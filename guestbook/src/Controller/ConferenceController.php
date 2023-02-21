2e3d5269183990d8c41bf5262184d9dfa0c9180d
ClementR48 <riera.clement@gmail.com>
2023-02-21 11:04:48 +0000

    Page admin ajoutée et CRUD mis en place

--- guestbook/src/Controller/ConferenceController.php
+++ guestbook/src/Controller/ConferenceController.php
@@ -4,13 +4,15 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/conference', name: 'app_conference')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        dd($request);
         return $this->render('conference/index.html.twig', [
             'controller_name' => 'ConferenceController',
         ]);
